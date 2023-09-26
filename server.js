2b9f54c96a01edd3eb7f029fbf13e9286666ce73
HyeeunKim07 <138830912+HyeeunKim07@users.noreply.github.com>
2023-09-26 22:37:43 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function(){
+    console.log('listening on 8080')
+});
+
+//누군가가 /pet 으로 방문을 하면..
+//pet관련된 안내문을 띄워주자
+
+app.get('/pet', function(req, res){
+    res.send('펫용품 쇼핑할 수 있는 페이지입니다.');
+});
+
+app.get('/beauty', function(req, res){
+    res.send('뷰티용품 쇼핑할 수 있는 페이지입니다.');
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+});
