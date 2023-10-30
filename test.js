89c4ff493dcaa5590e1a862c20974f4530316aa4
HyeeunKim07 <138830912+HyeeunKim07@users.noreply.github.com>
2023-10-30 20:36:14 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,164 @@
+const data = [
+    {
+        "ATPT_OFCDC_SC_CODE": "시도교육청코드",
+        "ATPT_OFCDC_SC_NM": "시도교육청명",
+        "SD_SCHUL_CODE": "행정표준코드",
+        "SCHUL_NM": "학교명",
+        "MMEAL_SC_CODE": "식사코드",
+        "MMEAL_SC_NM": "식사명",
+        "MLSV_YMD": "급식일자",
+        "MLSV_FGR": "급식인원수",
+        "DDISH_NM": "요리명",
+        "ORPLC_INFO": "원산지정보",
+        "CAL_INFO": "칼로리정보",
+        "NTR_INFO": "영양정보",
+        "LOAD_DTM": "수정일자"
+    },
+    {
+        "ATPT_OFCDC_SC_CODE": "P10",
+        "ATPT_OFCDC_SC_NM": "전라북도교육청",
+        "SD_SCHUL_CODE": "8320104",
+        "SCHUL_NM": "상산고등학교",
+        "MMEAL_SC_CODE": "1",
+        "MMEAL_SC_NM": "조식",
+        "MLSV_YMD": "20231017",
+        "MLSV_FGR": "1018.00",
+        "DDISH_NM": "잡곡밥/쌀밥 (5)<br/>모듬빵&잼 (1.2.5.6.13.14)<br/>순두부찌개 (5.9.13.18)<br/>고추잡채 (5.6.10.13.18)<br/>해쉬브라운/케찹 (5.6.12)<br/>포기김치 (9.13)<br/>찐옥수수 <br/>포도쥬스/우유 (2.13)<br/>망고요거트/누룽지 (2)<br/>오레오오즈/그래놀라코코아호두 (2.5.6)<br/>리치/방울토마토 (12)<br/>하이라이스 (5.6.10.12.13.16)",
+        "ORPLC_INFO": "쇠고기(종류) : 국내산(한우)<br/>쇠고기 식육가공품 : 국내산<br/>돼지고기 : 국내산<br/>돼지고기 식육가공품 : 국내산<br/>닭고기 : 국내산<br/>닭고기 식육가공품 : 국내산<br/>오리고기 : 국내산<br/>오리고기 가공품 : 국내산<br/>쌀 : 국내산<br/>배추 : 국내산<br/>고춧가루 : 국내산<br/>콩 : 국내산<br/>낙지 : 국내산<br/>고등어 : 국내산<br/>갈치 : 국내산<br/>오징어 : 국내산<br/>꽃게 : 국내산<br/>참조기 : 국내산<br/>비고 : ",
+        "CAL_INFO": "1590.3 Kcal",
+        "NTR_INFO": "탄수화물(g) : 226.8<br/>단백질(g) : 53.2<br/>지방(g) : 50.7<br/>비타민A(R.E) : 549.9<br/>티아민(mg) : 1.7<br/>리보플라빈(mg) : 1.3<br/>비타민C(mg) : 73.1<br/>칼슘(mg) : 2239.7<br/>철분(mg) : 8.1",
+        "LOAD_DTM": "20231016"
+    },
+    {
+        "ATPT_OFCDC_SC_CODE": "P10",
+        "ATPT_OFCDC_SC_NM": "전라북도교육청",
+        "SD_SCHUL_CODE": "8320104",
+        "SCHUL_NM": "상산고등학교",
+        "MMEAL_SC_CODE": "1",
+        "MMEAL_SC_NM": "조식",
+        "MLSV_YMD": "20231016",
+        "MLSV_FGR": "1018.00",
+        "DDISH_NM": "잡곡밥/쌀밥 (5)<br/>야채죽/누룽지 <br/>모듬빵&잼 (1.2.5.6.13.14)<br/>갈치무조림 (5.6)<br/>치킨너겟 (1.2.5.6.15)<br/>깍두기 (9.13)<br/>코코볼/아몬드후레이크 (2.5.6)<br/>팽이버섯버터구이 (2)<br/>오렌지쥬스/우유 (2.13)<br/>사과/바나나 <br/>배추국 (5.6)<br/>에그마요샐러드 (1.5)",
+        "ORPLC_INFO": "쇠고기(종류) : 국내산(한우)<br/>쇠고기 식육가공품 : 국내산<br/>돼지고기 : 국내산<br/>돼지고기 식육가공품 : 국내산<br/>닭고기 : 국내산<br/>닭고기 식육가공품 : 국내산<br/>오리고기 : 국내산<br/>오리고기 가공품 : 국내산<br/>쌀 : 국내산<br/>배추 : 국내산<br/>고춧가루 : 국내산<br/>콩 : 국내산<br/>낙지 : 국내산<br/>고등어 : 국내산<br/>갈치 : 국내산<br/>오징어 : 국내산<br/>꽃게 : 국내산<br/>참조기 : 국내산<br/>비고 : ",
+        "CAL_INFO": "1441.7 Kcal",
+        "NTR_INFO": "탄수화물(g) : 188.7<br/>단백질(g) : 52.8<br/>지방(g) : 51.8<br/>비타민A(R.E) : 373.3<br/>티아민(mg) : 0.9<br/>리보플라빈(mg) : 1.1<br/>비타민C(mg) : 52.1<br/>칼슘(mg) : 1358.5<br/>철분(mg) : 6.5",
+        "LOAD_DTM": "20231016"
+    },
+    {
+        "ATPT_OFCDC_SC_CODE": "P10",
+        "ATPT_OFCDC_SC_NM": "전라북도교육청",
+        "SD_SCHUL_CODE": "8320104",
+        "SCHUL_NM": "상산고등학교",
+        "MMEAL_SC_CODE": "2",
+        "MMEAL_SC_NM": "중식",
+        "MLSV_YMD": "20231017",
+        "MLSV_FGR": "1018.00",
+        "DDISH_NM": "붕어빵 (1.2.5.6)<br/>유부된장국 (5.6)<br/>콩조림 (5.6.13)<br/>닭다리후라이드 (6.15)<br/>깍두기 (9.13)<br/>포도 <br/>식혜 (13)<br/>옛날도시락 (1.2.5.6.9.10.13.15.16)",
+        "ORPLC_INFO": "쇠고기(종류) : 국내산(한우)<br/>쇠고기 식육가공품 : 국내산<br/>돼지고기 : 국내산<br/>돼지고기 식육가공품 : 국내산<br/>닭고기 : 국내산<br/>닭고기 식육가공품 : 국내산<br/>오리고기 : 국내산<br/>오리고기 가공품 : 국내산<br/>쌀 : 국내산<br/>배추 : 국내산<br/>고춧가루 : 국내산<br/>콩 : 국내산<br/>낙지 : 국내산<br/>고등어 : 국내산<br/>갈치 : 국내산<br/>오징어 : 국내산<br/>꽃게 : 국내산<br/>참조기 : 국내산<br/>비고 : ",
+        "CAL_INFO": "788.1 Kcal",
+        "NTR_INFO": "탄수화물(g) : 106.4<br/>단백질(g) : 38.6<br/>지방(g) : 22.8<br/>비타민A(R.E) : 206.6<br/>티아민(mg) : 0.4<br/>리보플라빈(mg) : 0.5<br/>비타민C(mg) : 32.1<br/>칼슘(mg) : 1228.1<br/>철분(mg) : 3.7",
+        "LOAD_DTM": "20231016"
+    },
+    {
+        "ATPT_OFCDC_SC_CODE": "P10",
+        "ATPT_OFCDC_SC_NM": "전라북도교육청",
+        "SD_SCHUL_CODE": "8320104",
+        "SCHUL_NM": "상산고등학교",
+        "MMEAL_SC_CODE": "2",
+        "MMEAL_SC_NM": "중식",
+        "MLSV_YMD": "20231016",
+        "MLSV_FGR": "1018.00",
+        "DDISH_NM": "락교&초생강 <br/>갈비만두 (1.5.6.10.16.18)<br/>포기김치 (9.13)<br/>매실쥬스 (13)<br/>파인애플볶음밥 (2.5.6.9.13.16.18)<br/>블루베리샐러드 (1.2.5.6)<br/>분짜 (5.6.10.13.15.16.18)<br/>쌀국수육수 (5.6.15.16.18)<br/>부시맨브레드 (1.2.5)",
+        "ORPLC_INFO": "쇠고기(종류) : 국내산(한우)<br/>쇠고기 식육가공품 : 국내산<br/>돼지고기 : 국내산<br/>돼지고기 식육가공품 : 국내산<br/>닭고기 : 국내산<br/>닭고기 식육가공품 : 국내산<br/>오리고기 : 국내산<br/>오리고기 가공품 : 국내산<br/>쌀 : 국내산<br/>배추 : 국내산<br/>고춧가루 : 국내산<br/>콩 : 국내산<br/>낙지 : 국내산<br/>고등어 : 국내산<br/>갈치 : 국내산<br/>오징어 : 국내산<br/>꽃게 : 국내산<br/>참조기 : 국내산<br/>비고 : ",
+        "CAL_INFO": "876.9 Kcal",
+        "NTR_INFO": "탄수화물(g) : 135.8<br/>단백질(g) : 33.8<br/>지방(g) : 22.2<br/>비타민A(R.E) : 285.2<br/>티아민(mg) : 0.7<br/>리보플라빈(mg) : 0.4<br/>비타민C(mg) : 34.3<br/>칼슘(mg) : 1470.2<br/>철분(mg) : 5.5",
+        "LOAD_DTM": "20231016"
+    },
+    {
+        "ATPT_OFCDC_SC_CODE": "P10",
+        "ATPT_OFCDC_SC_NM": "전라북도교육청",
+        "SD_SCHUL_CODE": "8320104",
+        "SCHUL_NM": "상산고등학교",
+        "MMEAL_SC_CODE": "3",
+        "MMEAL_SC_NM": "석식",
+        "MLSV_YMD": "20231017",
+        "MLSV_FGR": "1018.00",
+        "DDISH_NM": "쌀밥 <br/>얼큰수제비 (5.6)<br/>아삭이고추무침 (5.6)<br/>배추겉절이 (9.13)<br/>제육강정 (5.6.10.12.13)<br/>복숭아아이스티 (11)<br/>후르츠샐러드 (1.2.5.6.11)<br/>푸딩 ",
+        "ORPLC_INFO": "쇠고기(종류) : 국내산(한우)<br/>쇠고기 식육가공품 : 국내산<br/>돼지고기 : 국내산<br/>돼지고기 식육가공품 : 국내산<br/>닭고기 : 국내산<br/>닭고기 식육가공품 : 국내산<br/>오리고기 : 국내산<br/>오리고기 가공품 : 국내산<br/>쌀 : 국내산<br/>배추 : 국내산<br/>고춧가루 : 국내산<br/>콩 : 국내산<br/>낙지 : 국내산<br/>고등어 : 국내산<br/>갈치 : 국내산<br/>오징어 : 국내산<br/>꽃게 : 국내산<br/>참조기 : 국내산<br/>비고 : ",
+        "CAL_INFO": "1113.7 Kcal",
+        "NTR_INFO": "탄수화물(g) : 215.2<br/>단백질(g) : 28.1<br/>지방(g) : 16.8<br/>비타민A(R.E) : 221.9<br/>티아민(mg) : 1.0<br/>리보플라빈(mg) : 0.7<br/>비타민C(mg) : 48.2<br/>칼슘(mg) : 1293.1<br/>철분(mg) : 2.6",
+        "LOAD_DTM": "20231016"
+    },
+    {
+        "ATPT_OFCDC_SC_CODE": "P10",
+        "ATPT_OFCDC_SC_NM": "전라북도교육청",
+        "SD_SCHUL_CODE": "8320104",
+        "SCHUL_NM": "상산고등학교",
+        "MMEAL_SC_CODE": "3",
+        "MMEAL_SC_NM": "석식",
+        "MLSV_YMD": "20231016",
+        "MLSV_FGR": "1018.00",
+        "DDISH_NM": "수수밥/쌀밥 <br/>미역국 (18)<br/>닭갈비 (5.6.13.15)<br/>숙주나물 <br/>포기김치 (9.13)<br/>게토레이 <br/>땡초야채전 (1.5.6)<br/>콘샐러드 (1.5.13)",
+        "ORPLC_INFO": "쇠고기(종류) : 국내산(한우)<br/>쇠고기 식육가공품 : 국내산<br/>돼지고기 : 국내산<br/>돼지고기 식육가공품 : 국내산<br/>닭고기 : 국내산<br/>닭고기 식육가공품 : 국내산<br/>오리고기 : 국내산<br/>오리고기 가공품 : 국내산<br/>쌀 : 국내산<br/>배추 : 국내산<br/>고춧가루 : 국내산<br/>콩 : 국내산<br/>낙지 : 국내산<br/>고등어 : 국내산<br/>갈치 : 국내산<br/>오징어 : 국내산<br/>꽃게 : 국내산<br/>참조기 : 국내산<br/>비고 : ",
+        "CAL_INFO": "872.6 Kcal",
+        "NTR_INFO": "탄수화물(g) : 120.9<br/>단백질(g) : 41.1<br/>지방(g) : 23.9<br/>비타민A(R.E) : 382.2<br/>티아민(mg) : 0.6<br/>리보플라빈(mg) : 0.9<br/>비타민C(mg) : 25.0<br/>칼슘(mg) : 1178.6<br/>철분(mg) : 5.0",
+        "LOAD_DTM": "20231016"
+    }
+]
+
+// 숫자 알레르기 변환
+const allergyNames = {
+    1: '난류',
+    2: '우유',
+    3: '메밀',
+    4: '땅콩',
+    5: '대두',
+    6: '밀가루',
+    7: '고등어',
+    8: '게',
+    9: '새우',
+    10: '돼지고기',
+    11: '복숭아',
+    12: '토마토',
+    13: '아황산염',
+    14: '호두',
+    15: '닭고기',
+    16: '쇠고기',
+    17: '오징어',
+    18: '조개류(굴, 전복, 홍합 포함)',
+    19: '잣'
+};
+
+const parsedData = data.slice(1).map(item => {
+    return {
+        "type": item["MMEAL_SC_NM"],
+        "date": item["MLSV_YMD"],
+        //"menu": item["DDISH_NM"].replaceAll('1', '난류'),
+        "menu": convertAllergyNumberToKorean(item["DDISH_NM"])
+    };
+});
+
+function convertAllergyNumberToKorean(menu){
+    for (let i = Object.keys(allergyNames).length; i > 0; i--) {
+        menu = menu.replaceAll(i, allergyNames[i])
+    }
+    return menu
+}
+
+const breakfastMenu = parsedData.find(item => item.type === '조식' && item.date === '20231016').menu;
+const lunchMenu = parsedData.find(item => item.type === '중식' && item.date === '20231016').menu;
+const dinnerMenu = parsedData.find(item => item.type === '석식' && item.date === '20231016').menu;
+
+const allergyData = data.slice(1).map(item => {
+    return {
+        "allergy": item["DDISH_NM"]
+    };
+});
+
+const allergy = allergyData.find(item => item.allergy === '알레르기').allergy;
+
+console.log('parsedData=',parsedData);
+console.log('조식=',breakfastMenu);
+console.log('중식=',lunchMenu);
+console.log('석식=',dinnerMenu);
+console.log('알레르기=',allergy);
